910572347d5be9e1bf9081bf51e924df301b43c6
Sharvil <karwasharvil@gmail.com>
2022-08-12 11:36:27 +0530

    pivot index

---
+++ pivotIndex.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+
+        for(int i=0;i<nums.size();i++){
+            right += nums[i];
+        } 
+
+        for(int i=0;i<nums.size();i++){
+            if(left == right - nums[i]){
+                return i;
+            }
+            left += nums[i];
+            right -= nums[i];
+        }
+
+        return -1;
+    }
+};
