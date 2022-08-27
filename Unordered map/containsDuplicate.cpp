95df50fef87e05555c4b4dab5ac20a2bdf4370c7
Sharvil <karwasharvil@gmail.com>
2022-08-27 19:37:25 +0530

    cd

--- Unordered map/containsDuplicate.cpp
+++ Unordered map/containsDuplicate.cpp
@@ -3,13 +3,12 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> m;
         for(int j=0;j<nums.size();j++){
-            int i = nums[j];
-            if(m.find(i) == m.end()){
-                m[i] = j; 
+            if(m.find(nums[j]) == m.end()){
+                m[nums[j]] = j; 
             }
             else{
-                m[i] = j - m[i]; 
-                if(m[i] <= k){
+                m[nums[j]] = j - m[nums[j]]; 
+                if(m[nums[j]] <= k){
                     return true;
                 }
             }
