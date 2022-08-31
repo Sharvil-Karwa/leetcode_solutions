d1733259813bc04428def0fca103901c34acdfc4
Sharvil <karwasharvil@gmail.com>
2022-08-31 08:40:36 +0530

    move zeroes

---
+++ Vectors/moveZeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> temp;
+        for(auto i:nums){
+            if(i!=0)
+                temp.push_back(i);
+        }
+        for(int i=0;i<temp.size();i++){
+            nums[i] = temp[i];
+        }
+        for(int i=temp.size();i<nums.size();i++){
+            nums[i] = 0;
+        }
+    }
+};
