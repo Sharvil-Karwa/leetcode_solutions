42112964b51ad46274243154dcf4197f11158bb8
Sharvil <karwasharvil@gmail.com>
2022-09-18 18:53:15 +0530

    subsets

---
+++ Vectors/subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<pow(2,nums.size());i++){
+            vector<int> a;
+            for(int j=0;j<nums.size();j++){
+                if(i&(1<<j)){
+                    a.push_back(nums[j]);
+                }
+            }
+            ans.push_back(a);
+        }
+        
+        return ans;
+    }
+};
