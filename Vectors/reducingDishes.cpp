a4bc9924f255d1caaa9a0e4cbecac2c2058c42dc
Sharvil <karwasharvil@gmail.com>
2023-03-29 15:13:07 +0530

    max satisfaction

---
+++ Vectors/reducingDishes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(),satisfaction.end());
+        int sum=0;
+        int ans=0;
+        for(int i=satisfaction.size()-1;i>=0;i--){
+            int k = 1;
+            for(int j=i;j<satisfaction.size();j++){
+                sum+=k*satisfaction[j];
+                k++;
+            } 
+            if(sum<ans) return ans;
+            ans = sum;
+        }
+        return ans;
+    }
+};
