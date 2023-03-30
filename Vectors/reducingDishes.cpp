1fcb342c2d518a70bb70c9dd4c90d0c4e86f3bb2
Sharvil <karwasharvil@gmail.com>
2023-03-30 23:32:07 +0530

    reducing dishes

--- Vectors/reducingDishes.cpp
+++ Vectors/reducingDishes.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
-        int sum=0;
         int ans=0;
+        int sum=0;
         for(int i=satisfaction.size()-1;i>=0;i--){
             int k = 1;
             for(int j=i;j<satisfaction.size();j++){
