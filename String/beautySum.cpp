34cfc1f25d0b96fbb5ab794be849716da452bcbd
Sharvil <karwasharvil@gmail.com>
2023-05-01 11:20:28 +0530

    beauty sum

--- String/beautySum.cpp
+++ String/beautySum.cpp
@@ -3,12 +3,12 @@ public:
     int beautySum(string s) {
         int ans = 0;
         for (int i = 0; i < s.size(); i++) {
-            unordered_map<char, int> m;
-            m[s[i]]++;
+            unordered_map<char, int> map;
+            map[s[i]]++;
             for (int j = i + 1; j < s.size(); j++) {
-                m[s[j]]++;
+                map[s[j]]++;
                 int max_freq = 0, min_freq = INT_MAX;
-                for(auto i:m){
+                for(auto i:map){
                     max_freq = max(max_freq, i.second);
                     min_freq = min(min_freq, i.second);
                 }
